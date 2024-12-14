793044318974ae0fe5e61d7d73c00804f38a799b
muicgeta <akakkakaamma8182@gmail.com>
2024-12-14 09:02:44 +0300

    Add files via upload

---
+++ casery.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["NX_49"]
+casery = "NX_49"
+##ايدي مطور السورس
+caserid = 7069250607
+##قناه السورس
+source = "https://t.me/SSAAV13" 
+ch = "SSAAV13" 
+##جروب السورس
+group = "https://t.me/SSAAV13" 
+##لوجو السورس
+photosource = "https://t.me/SSAAV13/1237"
+##توكن الصانع
+bot_token="8188246369:AAHQlvVs2gP1Ai52rdTnuHstlAVjM9JmlD0"
+##جلسه الاشتراك
+bot_token2="AgG6bcAAvLheVe_y62GYmW6aKBUF4iu5T2hMvtD5HfKpFCjBJ48R-pxw3ct4t1tqcPiG57HGI7X57tyut4cU19Ckr2YVVNn9KfX_Md5CBzSlujVYPPoOjVjJQM3oR2f6Z2Yz2Twpj0W1Eo3CTUbMTM_ZzA086X15ejUEa8PGpmggv-2X8k4Mxdv5BStYhqe7YMnbWx1Sm20zYb9f-HFleqBOo0WXPRTCIua6G-KGMF4YU6sLlBi7Azubk-V_vPQAk3y3a-nFfCqZZ8esb-GteZclwssyULrGsnt1uWCQxsdrGkQlzzfoUWviK90wXbS6lp4e4RLAPSgkr8VSw5MUICU8HIUnjAAAAAGDppXOAA"
